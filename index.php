fa761d429b70bd10d2ac68c5cc4a5ccbdd2b7828
UzSid <umasood000@gmail.com>
2024-11-11 15:41:56 -0500

    Changes

--- index.php
+++ index.php
@@ -8,10 +8,57 @@
 		echo "Connection failed: " . $e->getMessage();
 	}
 	//if we got here, the connection was successful
-	$stmt = $conn->query("SELECT Name FROM donutdb.donuts");
+	$stmt = $conn->query("SELECT * FROM donutdb.donuts");
 	while ($row = $stmt->fetch())
 	{
+		$rows[] = $row;
 		echo $row['Name'] . "\n";
 	}
+	//$encodedData = json_encode($rows);
 	$conn = null;
+	$someVar = 1;
 ?>
+
+<script type="text/javascript">
+	var javaScriptVar = "<?php echo $someVar; ?>";
+</script>
+
+<html>
+<body>
+
+<p id="p1">Hello World!</p>
+
+<p id="p2">Hello World!</p>
+
+<script>document.getElementById("p1").innerHTML = "<?php echo $someVar; ?>"</script>
+
+<div id="like_button_container"></div>
+<script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
+
+<script type="text/javascript">
+    fetch($encodedData)
+    .then(response => response.json())
+    .then(json => {
+      // loop through the array and use the data property, "name"
+      // you could use a regular for loop also, but this syntax
+      // is preferred
+      let content = "";
+      json.forEach(person => {
+        content += person.Name;
+        content += " is from ";
+        content += person.Price;
+      }); //end foreach
+  });
+</script>
+
+
+<script type="text/javascript">
+	var rows = <?php echo json_encode($rows);?>;
+	document.getElementById("p2").innerHTML = rows.toString();
+</script>
+
+<script id="index_js" src="like_button.js" data-args="<?php echo $encodedData; ?>"></script>
+
+</body>
+</html>
--- like_button.js
+++ like_button.js
@@ -2,8 +2,23 @@ const e = React.createElement;
 
 class LikeButton extends React.Component {
   render() {
-    return 'amogus';
-  }
+    var script_tag = document.getElementById('index_js');
+    var redirectUrl = script_tag.getAttribute("data-args");
+    fetch(redirectUrl)
+    .then(response => response.json())
+    .then(json => {
+      // loop through the array and use the data property, "name"
+      // you could use a regular for loop also, but this syntax
+      // is preferred
+      let content = "";
+      json.forEach(person => {
+        content += person.Name;
+        content += " is from ";
+        content += person.Price;
+      }); //end foreach
+    return content;
+  });
+}
 }
 
 const domContainer = document.querySelector('#like_button_container');
