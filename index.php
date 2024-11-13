085f6a550fe237ac0a4e839471787badfed995e0
UzSid <umasood000@gmail.com>
2024-11-13 15:42:20 -0500

    work

--- index.php
+++ index.php
@@ -1,64 +1,54 @@
-		<?php //mysql:host.localhost:3306
+<?php
 	try {
 		$conn = new PDO('mysql:host=localhost;dbname=donutdb;', 'root', '');
-		//check the connection
-		// set the PDO error mode to exception
-		//echo "Connected successfully";
 	} catch(PDOException $e) {
 		echo "Connection failed: " . $e->getMessage();
 	}
-	//if we got here, the connection was successful
 	$stmt = $conn->query("SELECT * FROM donutdb.donuts");
 	while ($row = $stmt->fetch())
 	{
 		$rows[] = $row;
-		echo $row['Name'] . "\n";
 	}
-	//$encodedData = json_encode($rows);
-	$conn = null;
-	$someVar = 1;
+	$encodedData = json_encode($rows);
+	file_put_contents('data.json', $encodedData);
+	//$conn = null;
 ?>
 
-<script type="text/javascript">
-	var javaScriptVar = "<?php echo $someVar; ?>";
-</script>
-
 <html>
-<body>
+	<body>
+	<script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+	<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
 
-<p id="p1">Hello World!</p>
+	<p id="p2"></p>
 
-<p id="p2">Hello World!</p>
+	<div id="like_button_container"></div>
 
-<script>document.getElementById("p1").innerHTML = "<?php echo $someVar; ?>"</script>
+	<script>
+		fetch("data.json")
+		.then(response => response.json())
+		.then(json => {
+		let content = "";
+		json.forEach(donut => {
+			content += donut.Name;
+			content += " costs ";
+			content += donut.Price;
+			content += "<br><br>";
+		});
+		document.getElementById("p2").innerHTML = content;
+	});
 
-<div id="like_button_container"></div>
-<script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
-<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
+		const e = React.createElement;
 
-<script type="text/javascript">
-    fetch($encodedData)
-    .then(response => response.json())
-    .then(json => {
-      // loop through the array and use the data property, "name"
-      // you could use a regular for loop also, but this syntax
-      // is preferred
-      let content = "";
-      json.forEach(person => {
-        content += person.Name;
-        content += " is from ";
-        content += person.Price;
-      }); //end foreach
-  });
-</script>
+		class LikeButton extends React.Component {
+			render() {
+				return "redirectUrl";
+			}
+		}
 
+		const domContainer = document.querySelector('#like_button_container');
+		const root = ReactDOM.createRoot(domContainer);
+		root.render(e(LikeButton));
 
-<script type="text/javascript">
-	var rows = <?php echo json_encode($rows);?>;
-	document.getElementById("p2").innerHTML = rows.toString();
-</script>
-
-<script id="index_js" src="like_button.js" data-args="<?php echo $encodedData; ?>"></script>
-
-</body>
+	</script>
+	</body>
 </html>
--- like_button.js
+++ like_button.js
@@ -1,26 +1,55 @@
-const e = React.createElement;
+/*function MyComponent() {
+  const [data, setData] = useState([]);
 
-class LikeButton extends React.Component {
-  render() {
-    var script_tag = document.getElementById('index_js');
-    var redirectUrl = script_tag.getAttribute("data-args");
-    fetch(redirectUrl)
-    .then(response => response.json())
-    .then(json => {
-      // loop through the array and use the data property, "name"
-      // you could use a regular for loop also, but this syntax
-      // is preferred
-      let content = "";
-      json.forEach(person => {
-        content += person.Name;
-        content += " is from ";
-        content += person.Price;
-      }); //end foreach
-    return content;
-  });
-}
+  useEffect(() => {
+    setData([
+      { id: 1, title: 'Post 1', description: 'This is the first post.' },
+      { id: 2, title: 'Post 2', description: 'This is the second post.' }
+    ]);
+  }, []);
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 const domContainer = document.querySelector('#like_button_container');
 const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+root.render(e(LikeButton));*/
+
+/*const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));*/
+
+const root = ReactDOM.createRoot(
+  document.getElementById('like_button_container')
+);
+const element = <h1>Hello, world</h1>;
+root.render(element);
