b9429b5d49b47d24a92f46b1d09012073bbaa254
YooZee <umasood000@gmail.com>
2024-11-16 12:55:02 -0500

    getting there

--- app.js
+++ app.js
@@ -1,42 +1,26 @@
-import React, { Component } from 'react';
- // import logo from './logo.svg';
-import axios from 'axios'; //Import Axios
-import './App.css';
+fetch("http://localhost/projects/project3/index.php")
+    .then(response => response.json())
+    .then(json => {
+        sessionStorage.setItem("donutList", JSON.stringify(json));				
+    });
 
-class App extends Component{
-state = {
-  text : ""
-}
-handleAdd = async e =>{
- await this.setState({
-   text : e.target.value
- }) 
-}
-handleSubmit = e =>{
- e.preventDefault();
- console.log(this.state.text);
- let formData = new FormData();
- formData.append("text", this.state.text);
- const url = "http://localhost:80/react-backend/"
- axios.post(url,formData)
- .then(res=> console.log(res.data))
- .catch(err=> console.log(err))
-}
-render(){
-  return(
-    <div className="App-header">
-     <input 
-       onChange={this.handleAdd}
-       className="form-control" 
-       // value={this.state.text}
-       type="text" 
-       id='text' 
-       placeholder='Enter Some Text!'/> 
-       <br/>
-       <button 
-        onClick={this.handleSubmit}
-        className="btn btn-success" 
-        id='submit'>  Save</button>
-    </div>
-  );
-}
+let arr = JSON.parse(sessionStorage.getItem("donutList"));
+const donut = (
+    <ul>
+    { 
+        arr.map(({ID, Name, Price, Description}) => {      
+            return (
+                <div key={ID}>
+                    <p>{Name}</p>
+                    <p>{Price}</p>
+                    <p>{Description}</p>
+                </div>
+            )
+        })
+    }
+    </ul>
+);
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(donut);
--- index.php
+++ index.php
@@ -10,45 +10,6 @@
 		$rows[] = $row;
 	}
 	$encodedData = json_encode($rows);
-	file_put_contents('data.json', $encodedData);
-	//$conn = null;
+	$conn = null;
+	echo $encodedData;
 ?>
-
-<html>
-	<body>
-	<script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
-	<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
-
-	<p id="p2"></p>
-
-	<div id="like_button_container"></div>
-
-	<script>
-		fetch("data.json")
-		.then(response => response.json())
-		.then(json => {
-		let content = "";
-		json.forEach(donut => {
-			content += donut.Name;
-			content += " costs ";
-			content += donut.Price;
-			content += "<br><br>";
-		});
-		document.getElementById("p2").innerHTML = content;
-	});
-
-		const e = React.createElement;
-
-		class LikeButton extends React.Component {
-			render() {
-				return "redirectUrl";
-			}
-		}
-
-		const domContainer = document.querySelector('#like_button_container');
-		const root = ReactDOM.createRoot(domContainer);
-		root.render(e(LikeButton));
-
-	</script>
-	</body>
-</html>
