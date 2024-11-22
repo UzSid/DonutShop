2968277c024b9081594e6e1e26b1f5d333b01b7d
YooZee <umasood000@gmail.com>
2024-11-22 13:32:46 -0500

    FINISHED

--- donutdb.sql
+++ donutdb.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 06, 2024 at 08:14 PM
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 22, 2024 at 07:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,9 +39,17 @@ CREATE TABLE `donuts` (
 --
 
 INSERT INTO `donuts` (`ID`, `Name`, `Description`, `Price`) VALUES
-(1, 'Glazed Donut', 'Donut with sticky-sweet glaze', '$1.25'),
-(2, 'Jelly Donut', 'Donut stuffed with strawberry jelly', '$1.75'),
-(3, 'Chocolate-Frosted Donut', 'Donut covered in chocolate frosting', '$1.25');
+(1, 'Glazed Donut', 'Donut with sticky-sweet glaze', '$0.99'),
+(5, 'Jelly Donut', 'Donut stuffed with strawberry jelly', '$1.49'),
+(6, 'Chocolate-Frosted Donut', 'Donut covered in chocolate frosting', '$0.99'),
+(9, 'Maple Syrup Donut', 'Donut infused and glazed with genuine maple syrup', '$1.99'),
+(220, 'Mega Donut', 'A huge donut that is perfect for sharing with friends', '$4.99'),
+(221, 'Blueberry Donut', 'Donut flavored with real blueberries', '$1.49'),
+(222, 'Diet Donut', 'Donut with reduced sugar content', '$0.99'),
+(223, 'Mini donut', 'Small donut for small appetites', '$0.49'),
+(224, 'Cherry Donut', 'Cherry-flavored donut', '$1.49'),
+(225, 'Cake Donut', 'Donut made from cake batter', '$0.99'),
+(226, 'Matcha Donut', 'Matcha-flavored donut', '$1.49');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +69,7 @@ ALTER TABLE `donuts`
 -- AUTO_INCREMENT for table `donuts`
 --
 ALTER TABLE `donuts`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=227;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
